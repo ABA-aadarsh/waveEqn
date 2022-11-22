0e9eb77a0bc5715c7398622c94b68b86f017c716
Aadarsh <109805574+ABA-aadarsh@users.noreply.github.com>
2022-11-22 20:14:38 +0545

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,13 +1,22 @@
 const canvas=document.querySelector("canvas")
+const controlls=[...document.querySelectorAll(".controllers")]
+const init=()=>{
+    const amplitude=document.querySelector("#a")
+    const timePeriod=document.querySelector("#T")
+    const wavelength=document.querySelector("#l")
+    amplitude.value=25
+    timePeriod.value=6
+    wavelength.value=100
+}
 const WIDTH=1024
 const HEIGHT=576
 canvas.width=WIDTH
 canvas.height=HEIGHT
 const c=canvas.getContext("2d")
 const wave={
-    a:50,
-    T:5,   //seconds
-    l:300,  //pixels
+    a:25,
+    T:6,   //seconds
+    l:100,  //pixels
     time:0
 }
 wave.w=(2*Math.PI)/wave.T
@@ -18,7 +27,10 @@ class Particle{
         this.radius=radius
     }
     updatePosition(){
-        this.position.y=HEIGHT/2 +( wave.a*Math.sin( (wave.w*wave.time) - (wave.k*this.position.x) ) )
+        wave.w=(2*Math.PI)/wave.T
+        wave.k=(2*Math.PI)/wave.l
+        this.position.y=HEIGHT/2 +( wave.a*Math.sin( (wave.w*wave.time) + (wave.k*this.position.x) ) )
+        // this.position.y=HEIGHT/2 +( 2*wave.a*Math.sin(wave.w*wave.time)*Math.cos(wave.k*this.position.x) )
     }
     draw(){
         c.beginPath();
@@ -41,14 +53,15 @@ const createParticles=(n)=>{
     }
     return particles
 }
-const particles=createParticles(80)
+const particles=createParticles(60)
+init()
 const animate=setInterval(()=>{
     c.clearRect(0,0,WIDTH,HEIGHT)
     c.fillStyle="white"
     c.fillRect(0,0,WIDTH,HEIGHT)
     particles.forEach((particle,index)=>{
         c.fillStyle="blue"
-        if(index==50){
+        if(index==30){
             c.fillStyle="black"
         }
         particle.draw()
@@ -56,3 +69,9 @@ const animate=setInterval(()=>{
     })
     wave.time+=0.1
 },100)
+controlls.forEach((c)=>{
+    c.addEventListener("change",()=>{
+        const value=c.value
+        wave[c.id]=value
+    })
+})
